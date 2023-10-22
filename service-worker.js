2aa1d291282d2e2d46191cb6c39ab0eddc5fd0b2
James <wangruiyang4@gmail.com>
2023-10-22 16:31:32 +0800

    Add files via upload
    Signed-off-by: James <wangruiyang4@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.9afa2167110700e32224e67f17027e58.js"
+  "precache-manifest.7bb506c83fe9ed3591a7acd153aa0aeb.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "unlock-music"});
